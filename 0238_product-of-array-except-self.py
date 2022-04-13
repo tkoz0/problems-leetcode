c95e44a270f18761fc0f81636f25999592c73cc0
TKoz[0] <13438573+tkoz0@users.noreply.github.com>
2022-04-13 13:27:49 -0400

    "O(1)" space solution

--- 0238_product-of-array-except-self.py
+++ 0238_product-of-array-except-self.py
@@ -1,6 +1,18 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
+        ret = [1]*n
+        acc = 1
+        for i in range(n-1):
+            acc *= nums[i]
+            ret[i+1] *= acc
+        acc = 1
+        for i in range(n-1,0,-1):
+            acc *= nums[i]
+            ret[i-1] *= acc
+        return ret
+        '''
+        n = len(nums)
         left = [0]*n
         right = [0]*n
         left[0] = nums[0]
@@ -15,3 +27,4 @@ class Solution:
         for i in range(1,n-1):
             ret[i] = left[i-1]*right[i+1]
         return ret
+        '''
