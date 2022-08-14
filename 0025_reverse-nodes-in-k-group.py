0b0582aae69f0681bc0746ca73ae6765a1b0122f
TKoz[0] <13438573+tkoz0@users.noreply.github.com>
2022-08-13 21:07:25 -0400

    wrote with O(1) space

--- 0025_reverse-nodes-in-k-group.py
+++ 0025_reverse-nodes-in-k-group.py
@@ -5,22 +5,41 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        if not head:
-            return None
+        def rev_group(a,b):
+            #print(a.val,b.val if b else 'none')
+            # a = start ptr, b = end ptr
+            p = a
+            e = b
+            while p is not b:
+                n = p
+                p = p.next
+                n.next = e
+                e = n
+            return e
+        pprev = head
         p = head.next
         kk = k-1
-        while kk > 0 and p:
+        while kk > 0: # since n >= k
             kk -= 1
+            pprev = pprev.next
             p = p.next
-        if kk > 0: # not long enough to reverse
-            return head
-        tail_part = self.reverseKGroup(p,k) # apply to rest of list
-        end_p = p
-        p = head
-        while p is not end_p:
-            n = p
-            p = p.next
-            n.next = tail_part
-            tail_part = n
-        return tail_part
-
+        ret = rev_group(head,p)
+        #z=ret;zz=[]
+        #while z:zz.append(z.val);z=z.next
+        #print(zz)
+        last_ptr = head # last node in reversed group
+        while True: # reverse other groups
+            p = last_ptr.next
+            next_last_ptr = p
+            kk = k
+            while kk > 0 and p:
+                kk -= 1
+                p = p.next
+            if kk > 0: # not enough nodes
+                break
+            last_ptr.next = rev_group(last_ptr.next,p)
+            last_ptr = next_last_ptr
+            #z=ret;zz=[]
+            #while z:zz.append(z.val);z=z.next
+            #print(zz)
+        return ret
