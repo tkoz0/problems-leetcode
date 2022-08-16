0ffb677cb3c6b585901ebe9bf3cf1013373f23e5
TKoz[0] <13438573+tkoz0@users.noreply.github.com>
2022-08-16 15:11:25 -0400

    Create 0141_linked-list-cycle.py

---
+++ 0141_linked-list-cycle.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        nodes = set()
+        p = head
+        while p:
+            if p in nodes:
+                return True
+            nodes.add(p)
+            p = p.next
+        return False
