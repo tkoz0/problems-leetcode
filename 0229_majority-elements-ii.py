d45e238ed29fdfb1a3aceb74a6f292eefbee3f9b
TKoz[0] <13438573+tkoz0@users.noreply.github.com>
2022-08-16 18:56:40 -0400

    boyer-moore majority vote algorithm

--- 0229_majority-elements-ii.py
+++ 0229_majority-elements-ii.py
@@ -1,23 +1,24 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        if len(nums) < 3:
-            return list(set(nums))
-        n = len(nums)
-        # medians can be done in linear time, don't feel like coding it
-        # still not O(1) memory
-        medm = sorted(nums)[n//2]
-        medl = sorted(nums)[:n//2][(n//2)//2]
-        medr = sorted(nums)[n//2:][(n//2)//2]
-        countm = 0
-        countl = 0
-        countr = 0
-        maj = n//3
-        for z in nums:
-            if z == medm: countm += 1
-            if z == medl: countl += 1
-            if z == medr: countr += 1
+        n1,c1 = 1,0
+        n2,c2 = 2,0
+        for n in nums:
+            if n == n1: # current majority candidates
+                c1 += 1
+            elif n == n2:
+                c2 += 1
+            elif c1 == 0: # assign new majority candidate
+                n1,c1 = n,1
+            elif c2 == 0:
+                n2,c2 = n,1
+            else:
+                c1 -= 1
+                c2 -= 1
+            #print(n1,c1,n2,c2)
         ret = []
-        if countm > maj: ret.append(medm)
-        if countl > maj: ret.append(medl)
-        if countr > maj: ret.append(medr)
-        return list(set(ret))
+        maj = len(nums)//3
+        if nums.count(n1) > maj:
+            ret.append(n1)
+        if nums.count(n2) > maj:
+            ret.append(n2)
+        return ret
