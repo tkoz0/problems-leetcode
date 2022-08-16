1986a5ac8dd0905d827c7327548234d23e1e0d6b
TKoz[0] <13438573+tkoz0@users.noreply.github.com>
2022-08-15 21:06:18 -0400

    lower memory use

--- 0923_3sum-with-multiplicity.py
+++ 0923_3sum-with-multiplicity.py
@@ -1,26 +1,19 @@
 class Solution:
     def threeSumMulti(self, arr: List[int], target: int) -> int:
-        L = [[0]*(301)]
-        # L[i][j] = ways for 2 nums in arr[:i+1] to sum to j
-        prevcounts = {arr[0]:1}
-        for n in arr[1:]:
-            L.append(L[-1][:]) # if n not included
+        Lprev = [0]*301 # previous column needed
+        L = [0]*301 # L[i][j] = num pairs of arr[:i+1] summing to j
+        LL = [0]*301 # LL[i][j] = num triples of arr[:i+1] summing to j
+        # only storing last column, loop starts at i=2
+        prevcounts = {arr[0]:2} if arr[0]==arr[1] else {arr[0]:1,arr[1]:1}
+        Lprev[arr[0]+arr[1]] = 1
+        L[arr[0]+arr[1]] = 1
+        for n in arr[2:]:
             for m in prevcounts:
-                #print(len(L),len(L[-1]),m+n,prevcounts)
-                L[-1][m+n] += prevcounts[m]
-            L[-1] = [z%(10**9+7) for z in L[-1]]
+                L[m+n] += prevcounts[m]
             if n not in prevcounts:
                 prevcounts[n] = 0
             prevcounts[n] += 1
-        #for z in L: print('L',z[:20])
-        LL = [[0]*(301),[0]*(301)]
-        # LL[i][j] = ways for 3 nums in arr[:i+1] to sum to j
-        arr_i = 2
-        for n in arr[2:]:
-            LL.append(LL[-1][:])
             for i in range(target-n+1):
-                LL[-1][i+n] += L[arr_i-1][i]
-            LL[-1] = [z%(10**9+7) for z in LL[-1]]
-            arr_i += 1
-        #for z in LL: print('LL',z[:20])
-        return LL[-1][target]
+                LL[i+n] += Lprev[i]
+            Lprev = L[:]
+        return LL[target]%(10**9+7)
