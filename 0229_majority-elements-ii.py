549119943396849d681e5fd04dba7231207f737e
TKoz[0] <13438573+tkoz0@users.noreply.github.com>
2022-08-16 18:46:18 -0400

    Update 0229_majority-elements-ii.py

--- 0229_majority-elements-ii.py
+++ 0229_majority-elements-ii.py
@@ -1,9 +1,23 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        maj = len(nums)//3
-        freq = dict()
-        for n in nums:
-            if n not in freq:
-                freq[n] = 0
-            freq[n] += 1
-        return [k for k in freq if freq[k] > maj]
+        if len(nums) < 3:
+            return list(set(nums))
+        n = len(nums)
+        # medians can be done in linear time, don't feel like coding it
+        # still not O(1) memory
+        medm = sorted(nums)[n//2]
+        medl = sorted(nums)[:n//2][(n//2)//2]
+        medr = sorted(nums)[n//2:][(n//2)//2]
+        countm = 0
+        countl = 0
+        countr = 0
+        maj = n//3
+        for z in nums:
+            if z == medm: countm += 1
+            if z == medl: countl += 1
+            if z == medr: countr += 1
+        ret = []
+        if countm > maj: ret.append(medm)
+        if countl > maj: ret.append(medl)
+        if countr > maj: ret.append(medr)
+        return list(set(ret))
