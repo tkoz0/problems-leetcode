e30989e9888a8ea7d61cae15773ce738a440f64b
TKoz[0] <13438573+tkoz0@users.noreply.github.com>
2022-08-16 11:12:50 -0400

    preallocate array to make it even faster

--- 1310_xor-queries-of-a-subarray.py
+++ 1310_xor-queries-of-a-subarray.py
@@ -1,6 +1,6 @@
 class Solution:
     def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
-        cache = [0]
-        for n in arr:
-            cache.append(cache[-1]^n)
+        cache = [0]*(len(arr)+1)
+        for i,n in enumerate(arr):
+            cache[i+1] = cache[i]^n
         return [cache[j+1]^cache[i] for i,j in queries]
