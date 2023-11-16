e149237fc4afe1c0d306cdd9922242d2fb21d90a
TKoz[0] <13438573+tkoz0@users.noreply.github.com>
2023-11-16 18:08:05 -0500

    Create 0974_subarray-sums-divisible-by-k.py

---
+++ 0974_subarray-sums-divisible-by-k.py
@@ -0,0 +1,32 @@
+class Solution:
+    def divandconq(self,arr,k) -> int:
+        if len(arr) == 1:
+            if arr[0]%k == 0:
+                return 1
+            else:
+                return 0
+        else:
+            m = len(arr)//2
+            arr1 = arr[:m] # left
+            arr2 = arr[m:] # right
+            t1 = self.divandconq(arr1,k)
+            t2 = self.divandconq(arr2,k)
+            ret = t1 + t2 # subarrays not sharing between both halfs
+            lcache = dict() # map mod k val count arr1[i:m] subarrays
+            s = 0
+            for z in arr1[::-1]: # cache subarrays of arr1 including arr1[m-1]
+                s += z
+                if s%k not in lcache:
+                    lcache[s%k] = 0
+                lcache[s%k] += 1
+            s = 0
+            for z in arr2: # go through arr2 counting shared subarrays
+                s += z
+                y = k - (s%k) # sum needed to get mod k = 0
+                if y == k:
+                    y = 0
+                if y in lcache:
+                    ret += lcache[y]
+            return ret
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        return self.divandconq(nums,k)
